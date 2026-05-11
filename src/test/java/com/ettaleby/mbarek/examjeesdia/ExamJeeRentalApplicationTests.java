d1b28876994fba1bc4cb71f6b46e63d9e9efd7ef
HermesThemes <momoboogeyman2000@gmail.com>
2026-05-11 13:08:27 +0000

    creation de projet

---
+++ src/test/java/com/ettaleby/mbarek/examjeesdia/ExamJeeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ettaleby.mbarek.examjeesdia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamJeeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
